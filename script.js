64d252f40a8e0104fbad016733435716dd1c1cbc
Lucas <lucasleitep15@gmail.com>
2024-01-10 13:52:00 -0300

    Initial

---
+++ script.js
@@ -0,0 +1,13 @@
+function SingUp() {
+  const wrapper = document.querySelector('.wrapper')
+  wrapper.style.display = 'none';
+  const wrappersingup = document.querySelector('.wrappersingup')
+  wrappersingup.style.display = 'initial'
+}
+
+function Login() {
+  const wrapper = document.querySelector('.wrapper')
+  wrapper.style.display = 'initial';
+  const wrappersingup = document.querySelector('.wrappersingup')
+  wrappersingup.style.display = 'none'
+}
